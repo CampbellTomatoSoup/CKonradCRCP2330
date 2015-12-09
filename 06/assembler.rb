993b97bd1915bbe00737e3b23250d9a0cd47643a
CampbellTomatoSoup <campbell.konrad@gmail.com>
2015-12-09 15:40:05 -0600

    6.3.21 commit

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,9 +1,15 @@
 
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.exist?(ARGV[0]) && File.readable?(ARGV[0])) 
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
 	asm_filename = ARGV[0]
 	puts asm_filename
 else
 	abort("Usage: ./assembler.rb Prog.asm")
+end
+
+if (&& File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
+	puts "The contents of #{asm_filename}"
+else
+	abort("#{asm_filename} is not found or is unreadable.")
 end
 
