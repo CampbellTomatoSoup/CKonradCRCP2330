a86d97ef17747d8ef1e3587a5a134efd2faefae0
CampbellTomatoSoup <campbell.konrad@gmail.com>
2015-12-09 14:55:26 -0600

    6.2 commit

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,4 +1,7 @@
 
-puts "*Joey from Friends voice* How you doin'?"
-feeling = gets.chomp
-puts "Are you sure you're doin' #{feeling} ?"
+#puts "*Joey from Friends voice* How you doin'?"
+#feeling = gets.chomp
+#puts "Are you sure you're doin' #{feeling} ?"
+
+asm_filename = ARGV[0]
+puts asm_filename
