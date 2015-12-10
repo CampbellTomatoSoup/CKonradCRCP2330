b98c69802f3b8586238254eeab0da8011f527f58
CampbellTomatoSoup <campbell.konrad@gmail.com>
2015-12-09 18:25:46 -0600

    6.5.21

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -11,6 +11,13 @@ end
 
 #defining if the file is readable. Reading from the file
 
+def hack_filename(asm_filename)
+	asm_basename = File.basename(asm_filename, '.asm')
+	path = File.split(asm_filename)[0]
+	"#{path}/{asm_basename}.hack"
+
+end
+
 unless args_valid?
 	abort("Usage: ./assembler.rb Prog.asm")
 end
@@ -25,11 +32,10 @@ end
 
 # is the file isn't readable: abort and output the string
 
+#"best to keep it tight"
+
 File.open(asm_filename) do |asm_file|
-	asm_basename = File.basename(asm_filename, '.asm')
-	path = File.split(asm_filename)
-	hack_filename = "#{path}/#{asm_basename}.hack"
-	File.open(hack_filename, 'w') do |hack_file|
+	File.open(hack_filename(asm_filename), 'w') do |hack_file|
 		assmebler = Assember.new(asm_file, hack_file)
 		assembler.assemble!
 	end
@@ -38,3 +44,8 @@ end
 # sub nest = open up the .hack file and pass it to the assembler to translate 
 # reason for error: Assembler doesn't exist yet! This is our main. 
 # now we focus on Assembler object. 
+
+#overall, you check for the incoming arguments, see if they're readable, and then if they are
+#give them to the assembler.
+
+
