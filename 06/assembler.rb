b6d8dbea2f09b9baa3624e385a0c1ba6bca21bbc
CampbellTomatoSoup <campbell.konrad@gmail.com>
2015-12-09 19:13:43 -0600

    6.6.1

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,3 +1,13 @@
+class Assembler
+	def initialize(asm_file, hack_file)
+		@asm_file = asm_file
+		@hack_file = hack_file
+	end
+
+	def assemble!
+		puts @asm_file.read
+	end	
+end
 
 def args_valid?
 	ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
@@ -15,7 +25,6 @@ def hack_filename(asm_filename)
 	asm_basename = File.basename(asm_filename, '.asm')
 	path = File.split(asm_filename)[0]
 	"#{path}/{asm_basename}.hack"
-
 end
 
 unless args_valid?
@@ -36,7 +45,7 @@ end
 
 File.open(asm_filename) do |asm_file|
 	File.open(hack_filename(asm_filename), 'w') do |hack_file|
-		assmebler = Assember.new(asm_file, hack_file)
+		assembler = Assembler.new(asm_file, hack_file)
 		assembler.assemble!
 	end
 end
